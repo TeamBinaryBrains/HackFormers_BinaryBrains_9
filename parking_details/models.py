d21207c7a821d4326b25f0c48d9a1bddc1290b4c
Kolwankar-Siddhiraj <siddhirajk77@gmail.com>
2023-03-18 14:33:02 +0530

    added parking_details app

--- parking_details/models.py
+++ parking_details/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+from accounts.models import *
 # Create your models here.
+
+
+class ParkingPlace(models.Model):
+
+    provider = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True)
+    title = models.CharField(max_length=64, blank=True, null=True)
+    address_line_1 = models.CharField(max_length=512, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=512, blank=True, null=True)
+    city = models.CharField(max_length=32, blank=True, null=True)
+    state = models.CharField(max_length=32, blank=True, null=True)
+    pincode = models.CharField(max_length=8, blank=True, null=True)
+    rate_per_hr = models.IntegerField(default=30)
+    is_active = models.BooleanField(default=True)
+    is_available = models.BooleanField(default=True)
+
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+class ParkingTrack(models.Model):
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True)
+    parking_place = models.ForeignKey(ParkingPlace, on_delete=models.CASCADE, blank=True, null=True)
+    start_time = models.DateTimeField(blank=True, null=True)
+    end_time = models.DateTimeField(blank=True, null=True)
+    rate_per_hr = models.IntegerField(default=0)
+    approx_duration = models.IntegerField(default=0)
+    final_duration = models.IntegerField(default=0)
+    is_exited = models.BooleanField(default=False)
+    vehicle_type = models.CharField(max_length=32, blank=True, null=True)
+    vehicle_no = models.CharField(max_length=16, blank=True, null=True)
+    # state => requested | accepted | rejected 
+    state = models.CharField(max_length=16, blank=True, null=True)
+
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+
--- parking_details/views.py
+++ parking_details/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def ParkingPlaceDetails(request):
+
+    if request.method == "POST":
+        pass
+
+    
+    
+    return HttpResponse("UnAuthorized !")
